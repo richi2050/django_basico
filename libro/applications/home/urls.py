35c2fc052a1a57881ea1d0a18f5ff7684ece307c
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-28 13:59:37 -0500

    Vistas Genéricas en Django
    Vistas Genéricas en Django

--- libro/applications/home/urls.py
+++ libro/applications/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='home_app'
 
 urlpatterns = [
-     path('index',views.IndexView.as_view(), name='index')
+     path('index',views.IndexView.as_view(), name='index'),
+     path('libros',views.ListaLibros.as_view(), name='lista')
 ]
--- libro/applications/home/views.py
+++ libro/applications/home/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import (
-    TemplateView,)
+    TemplateView,
+    ListView)
 
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+
+class ListaLibros(ListView):
+    template_name = "home/libros.html"
+    queryset = ['codigo','codigo2','codigo3','codigo4']
+    context_object_name='libros'
     
 
 
