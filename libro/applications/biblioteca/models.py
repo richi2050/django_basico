2310fc8d75948ec73629d003c3511a537506ca20
Ricardo Lugo <ricardo.lugo.recillas@gmail.com>
2021-04-28 17:38:34 -0500

    models
    models

--- libro/applications/biblioteca/models.py
+++ libro/applications/biblioteca/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField('nombres', max_length=80)
+    nacionalidad = models.CharField(blank=true, max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
